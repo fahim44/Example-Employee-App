a718fcdd62b80ee0618273bd78bd8a70beaa225e
Fahim Salam Chowdhury <fahim.carbangla@gmail.com>
2019-10-29 19:51:37 +0600

    add search page

--- app/src/main/java/com/fahim/example_employee_app/ui/fragments/SearchFragment.kt
+++ app/src/main/java/com/fahim/example_employee_app/ui/fragments/SearchFragment.kt
@@ -4,25 +4,51 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.EditText
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.paging.PagedList
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.fahim.example_employee_app.R
+import com.fahim.example_employee_app.adapters.EmployeeListAdapter
+import com.fahim.example_employee_app.models.Employee
 import com.fahim.example_employee_app.viewmodels.SearchViewModel
 
 class SearchFragment : Fragment() {
 
     private lateinit var searchViewModel: SearchViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        searchViewModel =
-            ViewModelProviders.of(this).get(SearchViewModel::class.java)
+    private var recyclerView: RecyclerView? = null
+    private  var adapter: EmployeeListAdapter? = null
+    private  var observer: Observer<PagedList<Employee>>? = null
+    private var currentSearchedName = ""
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        searchViewModel = ViewModelProviders.of(this).get(SearchViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_search, container, false)
+
+        recyclerView = root.findViewById(R.id.rv) as RecyclerView
+        recyclerView?.layoutManager = LinearLayoutManager(context)
+
+        val et : EditText = root.findViewById(R.id.et_search)
+        val ib : ImageButton = root.findViewById(R.id.ib_search)
+        ib.setOnClickListener {
+            if (et.text != null && et.text.toString().trim()!="" && et.text.toString() != currentSearchedName){
+                adapter  = EmployeeListAdapter(searchViewModel)
+                observer?.let {
+                    searchViewModel.searchedEmployeeListLD?.removeObserver(it)
+                }
+                observer = Observer(adapter!!::submitList)
+                searchViewModel.search("%" + et.text.toString() + "%")
+                searchViewModel.searchedEmployeeListLD?.observe(this,observer!!)
+                recyclerView?.adapter = adapter
+                currentSearchedName = et.text.toString()
+            }
+        }
+
         return root
     }
 }
--- app/src/main/java/com/fahim/example_employee_app/viewmodels/SearchViewModel.kt
+++ app/src/main/java/com/fahim/example_employee_app/viewmodels/SearchViewModel.kt
@@ -3,20 +3,27 @@ package com.fahim.example_employee_app.viewmodels
 import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.paging.PagedList
 import com.fahim.example_employee_app.EmployeeApplication
 import com.fahim.example_employee_app.models.Employee
+import com.fahim.example_employee_app.repositories.EmployeeRepository
+import javax.inject.Inject
 
 class SearchViewModel(application: Application) : BaseViewModel(application) {
 
+    @Inject
+    lateinit var repository: EmployeeRepository
+
+    var searchedEmployeeListLD : LiveData<PagedList<Employee>>? = null
+
     init {
         (application as EmployeeApplication).component.inject(this)
+
     }
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+    fun search(name:String){
+        searchedEmployeeListLD = repository.getSearchedEmployeeList(name)
     }
-    val text: LiveData<String> = _text
 
     override fun onItemClick(employee: Employee) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
