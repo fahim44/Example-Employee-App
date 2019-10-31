964427ede33a407bf40b418030b1230ca9cf5549
Fahim Salam Chowdhury <fahim.carbangla@gmail.com>
2019-10-31 14:59:39 +0600

    add testcase

--- app/src/androidTest/java/com/fahim/example_employee_app/RoomDBTest.kt
+++ app/src/androidTest/java/com/fahim/example_employee_app/RoomDBTest.kt
@@ -2,8 +2,8 @@ package com.fahim.example_employee_app
 
 import android.content.Context
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
+import androidx.paging.Config
+import androidx.paging.toLiveData
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -18,39 +18,14 @@ import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import java.io.IOException
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.TimeUnit
 
 
 @RunWith(AndroidJUnit4::class)
 class RoomDBTest {
     @get:Rule val testRule = InstantTaskExecutorRule()
 
-    companion object {
-        fun <T> getValue(liveData: LiveData<T>): T {
-            val data = arrayOfNulls<Any>(1)
-            val latch = CountDownLatch(1)
-            val observer = object : Observer<T> {
-                override fun onChanged(o: T?) {
-                    data[0] = o
-                    latch.countDown()
-                    liveData.removeObserver(this)
-                }
-            }
-            liveData.observeForever(observer)
-            latch.await(2, TimeUnit.SECONDS)
-
-            @Suppress("UNCHECKED_CAST")
-            return data[0] as T
-        }
-    }
-
-
     private lateinit var dao : EmployeeDao
     private lateinit var db : EmployeeDatabase
-
-
-
 
     @Before
     fun setUp() {
@@ -70,16 +45,49 @@ class RoomDBTest {
 
     @Test
     @Throws(Exception::class)
-    fun insertData(){
+    fun insertData(){   //insert and get data test
         val expected = Employee(1,"fahim",100000.99f,21,5.0f)
         val id = dao.insert(expected)
-        val actual = getValue(dao.getEmployee(id[0].toInt()))
+        val actual = TestUtils.getValue(dao.getEmployee(id[0].toInt()))
 
         assertEquals(expected.name, actual.name)
         assertEquals(expected.id, actual.id)
         assertEquals(expected.salary,actual.salary)
         assertEquals(expected.rating,actual.rating)
         assertEquals(expected.age,actual.age)
+    }
+
+    @Test
+    fun insertMultipleData(){
+        val expected1 = Employee(1,"fahim",100000.99f,21,5.0f)
+
+        val expected2 = Employee(2,"rahul",10.93f,35,0.23f)
+
+        val expected3 = Employee(3,"piash",1000.65f,10,3.4f)
+
+        dao.insert(expected3,expected1,expected2)
+
+        val actualList = TestUtils.getValue(dao.getAllEmployees().toLiveData(Config(pageSize = 30,enablePlaceholders = false,maxSize = 1000)))
+
+        assertEquals(3,actualList.size)
+
+        assertEquals(expected1.name, actualList[0]?.name)
+        assertEquals(expected1.id, actualList[0]?.id)
+        assertEquals(expected1.salary,actualList[0]?.salary)
+        assertEquals(expected1.rating,actualList[0]?.rating)
+        assertEquals(expected1.age,actualList[0]?.age)
+
+        assertEquals(expected2.name, actualList[1]?.name)
+        assertEquals(expected2.id, actualList[1]?.id)
+        assertEquals(expected2.salary,actualList[1]?.salary)
+        assertEquals(expected2.rating,actualList[1]?.rating)
+        assertEquals(expected2.age,actualList[1]?.age)
+
+        assertEquals(expected3.name, actualList[2]?.name)
+        assertEquals(expected3.id, actualList[2]?.id)
+        assertEquals(expected3.salary,actualList[2]?.salary)
+        assertEquals(expected3.rating,actualList[2]?.rating)
+        assertEquals(expected3.age,actualList[2]?.age)
     }
 
 
@@ -95,7 +103,7 @@ class RoomDBTest {
         expected.salary = 223355.887f
         expected .uid = id[0].toInt()
         dao.update(expected)
-        val actual = getValue(dao.getEmployee(id[0].toInt()))
+        val actual = TestUtils.getValue(dao.getEmployee(id[0].toInt()))
 
         assertEquals(expected.name, actual.name)
         assertEquals(expected.id, actual.id)
@@ -111,10 +119,102 @@ class RoomDBTest {
         val id = dao.insert(expected)
         expected.uid = id[0].toInt()
         dao.delete(expected)
-        val actual = getValue(dao.getEmployee(id[0].toInt()))
+        val actual = TestUtils.getValue(dao.getEmployee(id[0].toInt()))
 
         assertNull(actual)
     }
 
+
+    @Test
+    fun updateRating(){
+        val insertedValue = Employee(1,"fahim",100000.99f,21,5.0f)
+        val id = dao.insert(insertedValue)
+        val rating = 2.01f
+        dao.updateRating(id[0].toInt(),rating)
+        val actual = TestUtils.getValue(dao.getEmployee(id[0].toInt()))
+
+        assertNotEquals(insertedValue.rating,actual.rating)
+        assertEquals(rating,actual.rating)
+    }
+
+    @Test
+    fun getAllData(){
+        val expected1 = Employee(1,"fahim",100000.99f,21,5.0f)
+        val id1 = dao.insert(expected1)
+        expected1.uid = id1[0].toInt()
+
+        val expected2 = Employee(2,"rahul",10.93f,35,0.23f)
+        val id2 = dao.insert(expected2)
+        expected2.uid = id2[0].toInt()
+
+        val expected3 = Employee(3,"piash",1000.65f,10,3.4f)
+        val id3 = dao.insert(expected3)
+        expected3.uid = id3[0].toInt()
+
+
+        val actualList = TestUtils.getValue(dao.getAllEmployees().toLiveData(Config(pageSize = 30,enablePlaceholders = false,maxSize = 1000)))
+
+        assertEquals(3,actualList.size)
+
+        assertEquals(expected1.uid, actualList[0]?.uid)
+        assertEquals(expected1.name, actualList[0]?.name)
+        assertEquals(expected1.id, actualList[0]?.id)
+        assertEquals(expected1.salary,actualList[0]?.salary)
+        assertEquals(expected1.rating,actualList[0]?.rating)
+        assertEquals(expected1.age,actualList[0]?.age)
+
+        assertEquals(expected2.uid, actualList[1]?.uid)
+        assertEquals(expected2.name, actualList[1]?.name)
+        assertEquals(expected2.id, actualList[1]?.id)
+        assertEquals(expected2.salary,actualList[1]?.salary)
+        assertEquals(expected2.rating,actualList[1]?.rating)
+        assertEquals(expected2.age,actualList[1]?.age)
+
+        assertEquals(expected3.uid, actualList[2]?.uid)
+        assertEquals(expected3.name, actualList[2]?.name)
+        assertEquals(expected3.id, actualList[2]?.id)
+        assertEquals(expected3.salary,actualList[2]?.salary)
+        assertEquals(expected3.rating,actualList[2]?.rating)
+        assertEquals(expected3.age,actualList[2]?.age)
+    }
+
+
+    @Test
+    fun getSearchedData(){
+        val expected1 = Employee(1,"abcd",100000.99f,21,2.4f)
+        val id1 = dao.insert(expected1)
+        expected1.uid = id1[0].toInt()
+
+        val expected2 = Employee(2,"bcf",10.93f,35,4.3f)
+        val id2 = dao.insert(expected2)
+        expected2.uid = id2[0].toInt()
+
+        val expected3 = Employee(3,"abeeeec",1000.65f,10,5.0f)
+        val id3 = dao.insert(expected3)
+        expected3.uid = id3[0].toInt()
+
+
+        val actualList = TestUtils.getValue(dao.employeesSortByName("%bc%").toLiveData(Config(pageSize = 30,enablePlaceholders = false,maxSize = 1000)))
+
+        assertEquals(2,actualList.size)
+
+        assertFalse(actualList.contains(expected3))
+        assertTrue(actualList.contains(expected1))
+        assertTrue(actualList.contains(expected2))
+
+        assertEquals(expected2.uid, actualList[0]?.uid)
+        assertEquals(expected2.name, actualList[0]?.name)
+        assertEquals(expected2.id, actualList[0]?.id)
+        assertEquals(expected2.salary,actualList[0]?.salary)
+        assertEquals(expected2.rating,actualList[0]?.rating)
+        assertEquals(expected2.age,actualList[0]?.age)
+
+        assertEquals(expected1.uid, actualList[1]?.uid)
+        assertEquals(expected1.name, actualList[1]?.name)
+        assertEquals(expected1.id, actualList[1]?.id)
+        assertEquals(expected1.salary,actualList[1]?.salary)
+        assertEquals(expected1.rating,actualList[1]?.rating)
+        assertEquals(expected1.age,actualList[1]?.age)
+    }
 
 }
